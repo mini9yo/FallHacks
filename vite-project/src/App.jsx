cbc39150e4547886e8288bc6cf31524d5a06c6b2
Puneet North <puneetnorth170@gmail.com>
2024-09-28 12:03:05 -0700

    delete some duplicates

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from "./components/Home"
-
-function App() {
-  const [stops, setStops] = useState([]); 
-  const [loading, setLoading] = useState(true); 
-  const [error, setError] = useState(null); 
-
-  // useEffect(() => {
-  //   fetch('http://localhost:8080/api/stops') 
-  //       .then((response) => {
-  //           if (!response.ok) {
-  //               throw new Error('Network response was not ok');
-  //           }
-  //           return response.json();
-  //       })
-  //       .then((data) => {
-  //           setStops(data); 
-  //           setLoading(false); 
-  //       })
-  //       .catch((error) => {
-  //           setError(error); 
-  //           setLoading(false); 
-  //       });
-  // }, []); 
-=======
 import React from 'react';
 import Home from './components/Home'; // Correct import
->>>>>>> refs/remotes/origin/main
 
 const App = () => {
   return (
